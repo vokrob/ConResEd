f53c01c3407f288008a3e7d0824b1a184d12a76e
Andreev Konstantin <156372707+KostyanAndreev@users.noreply.github.com>
2026-04-15 13:38:24 +0700

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": { target: "http://127.0.0.1:8080", changeOrigin: true },
-      "/templates": { target: "http://127.0.0.1:8080", changeOrigin: true },
     },
   },
 });
