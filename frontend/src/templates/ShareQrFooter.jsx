fcdc5aca64c29634b4a4e08c6190defdf81f855f
Andreev Konstantin <156372707+KostyanAndreev@users.noreply.github.com>
2026-04-22 13:45:42 +0700

    Add files via upload

--- frontend/src/templates/ShareQrFooter.jsx
+++ frontend/src/templates/ShareQrFooter.jsx
@@ -6,13 +6,13 @@ export function ShareQrFooter({ publicUrl }) {
   return (
     <section className="share-qr-footer">
       <div className="share-qr-card">
-        <div className="share-qr-copy">
+	  {/* <div className="share-qr-copy">
           <a className="share-qr-link" href={publicUrl} target="_blank" rel="noreferrer">
             {publicUrl}
           </a>
-        </div>
+	  </div> */}
         <div className="share-qr-image">
-          <QrCode text={publicUrl} size={160} />
+          <QrCode text={publicUrl} size={200} />
         </div>
       </div>
     </section>
